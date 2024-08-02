25c08dbb17efc93495edfe5d9da677c339709f4a
Karen Fonseca <149629605+Karenfonseca22@users.noreply.github.com>
2024-08-02 10:56:52 -0500

    Create discrepant_values.sql

---
+++ Querys_sql/discrepant_values.sql
@@ -0,0 +1,20 @@
+--- Unificar variables STRING, como que todas queden en mayusculas, minusculas, que no haya una palabra que quede desogranizada
+--- Se verifico la categoria de prestamos.
+WITH clean_table AS (
+  SELECT
+    user_id,
+    CASE
+      WHEN LOWER(loan_type) = 'others' THEN 'other'
+      ELSE LOWER(loan_type)
+    END AS clean_loan_type
+  FROM
+    `riesgo-relativo-429614.Dataset.loans_pendientes`
+)
+SELECT
+  user_id,
+  SUM(CASE WHEN clean_loan_type = 'real estate' THEN 1 ELSE 0 END) AS total_real_estate_loans,
+  SUM(CASE WHEN clean_loan_type = 'other' THEN 1 ELSE 0 END) AS total_other_loans
+FROM
+  clean_table
+GROUP BY
+  user_id;
