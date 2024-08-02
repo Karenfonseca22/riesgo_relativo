9bddc973b77e83cdca4be5b250df35260614d0a9
Karen Fonseca <149629605+Karenfonseca22@users.noreply.github.com>
2024-08-02 10:54:00 -0500

    Create IQR.sql

---
+++ Querys_sql/IQR.sql
@@ -0,0 +1,42 @@
+--- Formula base y su aplicación de rango intercuartil para variable IQR
+WITH
+  quartiles AS (
+  SELECT
+    APPROX_QUANTILES(last_month_salary, 4)[
+  OFFSET
+    (1)] AS Q1,
+    APPROX_QUANTILES(last_month_salary, 4)[
+  OFFSET
+    (3)] AS Q3
+  
+  FROM
+    `riesgo-relativo-429614.Dataset.user_info_flag` ),
+  rango_inter AS (
+  SELECT
+    Q1,
+    Q3,
+    CAST(Q3 - Q1 AS FLOAT64) AS IQR
+  FROM
+    quartiles ),
+  limite AS (
+  SELECT
+    Q1,
+    Q3,
+    IQR,
+    Q1 - 1.5 * IQR AS limite_inferior,
+    Q3 + 1.5 * IQR AS limite_superior
+  FROM
+    rango_inter)
+SELECT
+  ld.*,
+  CASE
+    WHEN ld.last_month_salary < limite.limite_inferior THEN 'outlier_below'
+    WHEN ld.last_month_salary > limite.limite_superior THEN 'outlier_above'
+    ELSE 'not_outlier'
+END
+  AS outlier_last_month_salary
+FROM
+  `riesgo-relativo-429614.Dataset.user_info_flag` AS ld,
+  limite
+ORDER BY
+  ld.last_month_salary;
